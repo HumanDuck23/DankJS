3eb8fd31a9f030c0c66d7a92394f05dc65aac99c
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-04-22 13:12:40 +0200

    created Cache.ts

---
+++ src/discord/utils/Cache.ts
@@ -0,0 +1,28 @@
+import * as fs from "fs"
+
+/**
+ * Small Cache Class
+ */
+export class Cache {
+
+    file: string = "cache.json"
+
+    get(key: string): any {
+        return this.getCache()[key]
+    }
+
+    set(key: string, value: any): void {
+        const cache = this.getCache()
+        cache[key] = value
+        this.setCache(cache)
+    }
+
+    getCache(): any {
+        return JSON.parse(fs.readFileSync(this.file, "utf8"))
+    }
+
+    setCache(cache: any): void {
+        fs.writeFileSync(this.file, JSON.stringify(cache))
+    }
+
+}
